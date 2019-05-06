766816809017e9448c85f4708a518259cd6a3b3a
Luciano Fusco <luciano.fusco@nttdata.com>
2019-05-06 09:51:39 +0200

    fix per mail multiple
    fix per mail multiple

--- utils/mailManager.js
+++ utils/mailManager.js
@@ -109,6 +109,8 @@ function notifyUpdateClassfica(stagione) {
         oggi.getFullYear();
         
         const mailServer = {
+            pool: true, // pooled connections
+            maxConnections: 2,
             host: 'smtps.aruba.it',
             port: 465,
             secure: true,
@@ -118,22 +120,29 @@ function notifyUpdateClassfica(stagione) {
             }
         };
 
-        const transporter = nodeMailer.createTransport(mailServer);        
+        const transporter = nodeMailer.createTransport(mailServer);  
+        let mails = [];      
 
         for( let i = 0; i < emailAddress.length; i++){
             
             if (!stringIsNullOrWhiteSpace(emailAddress[i].email_address) && emailAddress[i].email_address !== 'abco@ciao.it'){
 
-                let mailOptions = {                
+                mails.push({                
                     from: '"Pronostici Lega Forum" <' + 'sorteggio@legaforum.com' + ' >', // sender address
                     to: emailAddress[i].email_address, // receiver in to
                     subject: 'Notifica Aggiornamento Classifica al ' + dateString, // Subject line
                     text: 'La classfica generale è stata aggiornata al ' + dateString, // plain text body
                     html: '<b>' + 'La classfica generale è stata aggiornata al ' + dateString + '</b>', // html body
                     attachments: []
-                };        
-                
-                transporter.sendMail(mailOptions, (error, info) => {
+                });
+            }
+        }
+
+        transporter.on("idle", function() {
+            // send next message from the pending queue
+            while (transporter.isIdle() && mails.length) {
+
+                transporter.sendMail(mails.shift(), (error, info) => {
 
                     if (error) {
                         console.log(error);
@@ -141,10 +150,11 @@ function notifyUpdateClassfica(stagione) {
                         console.log('Message %s sent: %s', info.messageId, info.response);
                     }
             
-                });                     
-            
+                });                   
             }
-        }
+        });
+
+        transporter.close();
 
     })
     .catch(error => { //gestione errore
@@ -255,6 +265,8 @@ function notifyNewSchedina(schedina) {
     dbCallSchedine.getEmailAddressPartecipanti().then(function(emailAddress){ //torna una promise
         
         const mailServer = {
+            pool: true,
+            maxConnections: 2,
             host: 'smtps.aruba.it',
             port: 465,
             secure: true,
@@ -265,31 +277,42 @@ function notifyNewSchedina(schedina) {
         };
 
         const transporter = nodeMailer.createTransport(mailServer);
-      
+        let mails = [];      
     
         for( let i = 0; i < emailAddress.length; i++){
 
             if (!stringIsNullOrWhiteSpace(emailAddress[i].email_address) && emailAddress[i].email_address !== 'abco@ciao.it'){
-                let mailOptions = {
+
+                mails.push({                
                     from: '"Pronostici Lega Forum" <' + 'sorteggio@legaforum.com' + ' >', // sender address
                     to: emailAddress[i].email_address, // list of receivers in bcc
                     subject: 'Notifica apertura nuova Schedina Settimanale Numero ' + schedina.settimana, // Subject line
                     text: 'Notifica apertura nuova Schedina Settimanale Numero ' + schedina.settimana, // plain text body
                     html: '<b>' + 'Notifica apertura nuova Schedina Settimanale Numero ' + schedina.settimana + '</b>', // html body
                     attachments: []
-                };                  
-                transporter.sendMail(mailOptions, ( error, info ) => {
+                });
+        
+            }
+        
+        }
+
+        transporter.on("idle", function() {
+            // send next message from the pending queue
+            while (transporter.isIdle() && mails.length) {
+
+                transporter.sendMail(mails.shift(), (error, info) => {
 
                     if (error) {
                         console.log(error);
                     }else{
                         console.log('Message %s sent: %s', info.messageId, info.response);
                     }
-                
-                });        
-        
+            
+                });                   
             }
-        }
+        });
+
+        transporter.close();        
 
     })
     .catch(error => { //gestione errore
